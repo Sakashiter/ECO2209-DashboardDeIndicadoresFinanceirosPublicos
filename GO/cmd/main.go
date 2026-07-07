d90941f77b08abd7b7be6a1a3175f7f7f0b24c16
Sakashiter <felipesakashita@gmail.com>
2026-07-07 18:03:32 -0300

    handlers main.go

--- GO/cmd/main.go
+++ GO/cmd/main.go
@@ -18,3 +18,36 @@ var indicadoresGlobais = []IndicadorFinanceiro{
 	{2, "IPCA", 0.45, "2026-06-30"},
 	{3, "Dolar", 5.42, "2026-07-07"},
 }
+
+func getIndicadoresHandler(w http.ResponseWriter, r *http.Request) {
+	// Apenas para fins de teste no terminal
+	fmt.Println("Requisição recebida na rota /api/indicadores")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`[{"id": 1, "tipo": "SELIC", "valor": 10.5, "data": "2026-07-07"}]`))
+}
+
+func exportCSVHandler(w http.ResponseWriter, r *http.Request) {
+	// Define os headers para forçar o download no navegador
+	w.Header().Set("Content-Type", "text/csv")
+	w.Header().Set("Content-Disposition", "attachment;filename=indicadores.csv")
+
+	writer := csv.NewWriter(w)
+	defer writer.Flush()
+
+	// Escreve o cabeçalho
+	writer.Write([]string{"Tipo", "Valor", "Data"})
+
+	// Itera sobre seus dados e escreve as linhas
+	// writer.Write([]string{ind.Tipo, fmt.Sprintf("%f", ind.Valor), ind.Data})
+	for _, ind := range indicadoresGlobais {
+		linha := []string{
+			fmt.Sprintf("%d", ind.ID),
+			ind.Tipo,
+			fmt.Sprintf("%.2f", ind.Valor), // Formata o float para 2 casas decimais
+			ind.Data,
+		}
+		writer.Write(linha)
+	}
+}
