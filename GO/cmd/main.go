fdf1c27c405498257b675784ae8d79f3c4d5574b
Sakashiter <felipesakashita@gmail.com>
2026-07-07 18:01:17 -0300

    IndicadoresGlobais main.go

--- GO/cmd/main.go
+++ GO/cmd/main.go
@@ -12,3 +12,9 @@ type IndicadorFinanceiro struct {
 	Valor float64 `json:"valor"`
 	Data  string  `json:"data"`
 }
+
+var indicadoresGlobais = []IndicadorFinanceiro{
+	{1, "SELIC", 10.50, "2026-07-07"},
+	{2, "IPCA", 0.45, "2026-06-30"},
+	{3, "Dolar", 5.42, "2026-07-07"},
+}
